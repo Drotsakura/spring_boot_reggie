0ca26585a11d1c9b3ff53a39ad69d66d10ca89fc
drotsakura <1611547464@qq.com>
2022-04-25 21:33:31 +0800

    第二次提交

--- src/main/java/com/drotsakura/SpringBootReggieApplication.java
+++ src/main/java/com/drotsakura/SpringBootReggieApplication.java
@@ -2,8 +2,10 @@ package com.drotsakura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootReggieApplication {
 
     public static void main(String[] args) {
